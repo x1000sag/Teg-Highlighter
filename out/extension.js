ad7f9fa582fde5193a3a4de8e0fa532331182982
x1000sa <x1000.sa@yandex.ru>
2025-10-31 13:44:51 +0300

    extension intialized

---
+++ out/extension.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+}
+// This method is called when your extension is deactivated
+function deactivate() { }
+//# sourceMappingURL=extension.js.map
